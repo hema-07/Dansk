aa70bfd72915e8b48629320a3b7b8bf1d04c2a7f
Hemavathi <hemavathi.tamilmaran95@gmail.com>
2018-03-22 18:27:31 +0530

    Update url in Dansk

--- server.js
+++ server.js
@@ -4,17 +4,28 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 var PORT = process.env.PORT || 8001;
+var  DilbertURL = 'http://Dilbert.com/strip/';
+
 app.get('/scrape', function(req, res){
   url = 'https://www.iposen.dk/shop/display/products?offers=1&category_id=41';
 
   request(url, function(error, response, html){
     if(!error){
-        var $ = cheerio.load(html),$header = $('title').html();
+        var $ = cheerio.load(html);
         var price= [], title = [], margin = [25,34,56,61,68,72,45,63,83],
         currentPrice = [6,7,5,8,5,6,21,26,29], newPrice = [4,5,4,6,4,3,18,17,18]
         outOftheDoor = [4,5,3,5,2,4,15,18,17];
         var data, data1, out=[],product_name=[],product_price_description=[];
-        var product_price = [], companyName = [];
+        var product_price = [], companyName = [] ,
+        image = ["http://www.freepngimg.com/download/orange/3-2-orange-png-image.png",
+                  "http://www.freepngimg.com/download/broccoli/3-2-broccoli-transparent.png",
+                  "http://www.pngmart.com/files/1/Green-Grapes-PNG.png",
+                  "https://meny.dk/sites/meny.dk/files/styles/product_image/public/z_original_252_passionsfrugt.png?itok=p1yhUzNy",
+                  "http://www.freepngimg.com/download/broccoli/3-2-broccoli-transparent.png",
+                  "http://www.freepngimg.com/download/broccoli/3-2-broccoli-transparent.png",
+                "http://earthcontrol.dk/wp-content/uploads/2014/05/EC-pine-kernels-green.png",
+              "https://vignette.wikia.nocookie.net/aonoexorcist/images/f/fb/Pineapple.png/revision/latest?cb=20141210014032",
+            "http://pngimg.com/uploads/watermelon/watermelon_PNG2660.png"];
         var json = {name : "", price : ""};
       $('.product_list_name').filter(function(){
         data = $(this);
@@ -35,6 +46,12 @@ app.get('/scrape', function(req, res){
           companyName.push(company);
       })
 
+      // $('.thumbnail').filter(function(){
+      //     data3 = $(this);
+      //     console.log(data3);
+      //     var images = data3.text().trim();
+      //     image.push(images);
+      // })
       product_price_description.map((item)=>{
         item = item.replace(/\r?\n|\r/g, " ");
         product_price.push(item.substr(0,6));
@@ -49,7 +66,8 @@ app.get('/scrape', function(req, res){
           'margin': margin[key],
           'currentPrice': currentPrice[key],
           'newPrice': newPrice[key],
-          'outOftheDoor': outOftheDoor[key]
+          'outOftheDoor': outOftheDoor[key],
+          'url' : image[key]
         });
         })
     }
